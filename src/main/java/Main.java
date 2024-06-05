0a1d0594be4d29009351268b1e404134eb975189
ByeongGyu Jeon <cube9928@naver.com>
2024-06-05 23:27:26 +0900

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,41 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
 
-     try {
-       serverSocket = new ServerSocket(4221);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write(
-               "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n".getBytes());
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
 
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+        try {
+            serverSocket = new ServerSocket(4221);
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept();
+
+
+            InputStream clientInput = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientInput));
+            OutputStream output = clientSocket.getOutputStream();
+
+
+            String input = reader.readLine();
+            String[] httpRequest = input.split(" ");
+
+            for(String arr : httpRequest){
+                System.out.println(arr);
+            }
+            if(httpRequest[1].equals("/")){
+                output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            }else{
+                output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+            }
+
+            System.out.println("accepted new connection");
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
