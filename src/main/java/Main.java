55b8f8065751fdf0456044456109b55875d7e4ff
ByeongGyu Jeon <cube9928@naver.com>
2024-06-22 18:07:15 +0900

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,18 +19,26 @@ public class Main {
             InputStream clientInput = clientSocket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(clientInput));
             OutputStream output = clientSocket.getOutputStream();
-
+            PrintStream outputWriter = new PrintStream(output);
 
             String input = reader.readLine();
             String[] httpRequest = input.split(" ");
 
+            // 콘솔 로그
             for(String arr : httpRequest){
                 System.out.println(arr);
             }
 
             if(httpRequest[1].contains("/echo")){
                 String[] url = httpRequest[1].split("/");
-                output.write(("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n\r\nbContent-Length: 3\r\n\r\n" + url[1]).getBytes());
+                //output.write(("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n\r\nbContent-Length: 3\r\n\r\n" + url[1]).getBytes());
+
+                outputWriter.println("HTTP/1.1 200 OK");
+                outputWriter.println("Content-Type: text/plain");
+                outputWriter.println("Content-Length: 3");
+                outputWriter.println(url[1]);
+
+
 
             } else if (httpRequest[1].equals("/")) {
                 output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
